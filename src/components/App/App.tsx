482f61b75a6d54ee9e7e2d4302234e08b066f62a
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-06 20:29:33 +0100

    Add App.tsx

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "../../pages/HomePage";
+
+const App = (): React.ReactElement => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/films" />} />
+      <Route path="/films" element={<HomePage />} />
+    </Routes>
+  );
+};
+export default App;
