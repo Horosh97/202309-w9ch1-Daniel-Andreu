939a2bce4fb7cd862a470a9f5b2b13118e992275
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-07 07:44:34 +0100

    Add loadMovies to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+import MoviesList from "../components/MoviesList/MoviesList";
+import { useAppDispatch } from "../store/hooks";
+import useMoviesApi from "../hooks/useMoviesApi";
+import { loadMoviesActionCreator } from "../store/features/movies/moviesSlice";
+
 const HomePage = (): React.ReactElement => {
-  return <main></main>;
+  const dispatch = useAppDispatch();
+  const { getMovies } = useMoviesApi();
+
+  useEffect(() => {
+    (async () => {
+      const actualMovies = await getMovies();
+      dispatch(loadMoviesActionCreator(actualMovies));
+    })();
+  }, [dispatch, getMovies]);
+
+  return (
+    <main>
+      <MoviesList />
+    </main>
+  );
 };
 export default HomePage;
