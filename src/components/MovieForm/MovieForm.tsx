284dc5461a629aa08442f9e2d4a5e3d245b9b265
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-09 18:16:22 +0100

    Add functionality to MovieForm

--- src/components/MovieForm/MovieForm.tsx
+++ src/components/MovieForm/MovieForm.tsx
@@ -1,14 +1,55 @@
+import { useState } from "react";
+import { MovieStructure } from "../../store/features/movies/types";
 import Button from "../Button/Button";
 import MovieFormStyled from "./MovieFormStyled";
 
-const MovieForm = (): React.ReactElement => {
+interface MovieFormProps {
+  actionOnSubmit: (movie: MovieStructure) => void;
+}
+
+const MovieForm = ({ actionOnSubmit }: MovieFormProps): React.ReactElement => {
+  const initialNewMovie: MovieStructure = {
+    id: 0,
+    title: "",
+    director: "",
+    image: "",
+    isWatched: false,
+  };
+
+  const [newMovie, setNewMovie] = useState(initialNewMovie);
+
+  const onChangeData = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewMovie((newMovie: MovieStructure) => ({
+      ...newMovie,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    actionOnSubmit(newMovie);
+
+    setNewMovie(initialNewMovie);
+  };
+
   return (
-    <MovieFormStyled className="movie-form">
+    <MovieFormStyled
+      className="movie-form"
+      autoComplete="off"
+      onSubmit={onFormSubmit}
+    >
       <div className="movie-form__block">
         <label className="movie-form__label" htmlFor="title">
           Title:
         </label>
-        <input id="title" className="movie-form__input" required type="text" />
+        <input
+          id="title"
+          className="movie-form__input"
+          required
+          type="text"
+          onChange={onChangeData}
+        />
       </div>
       <div className="movie-form__block">
         <label className="movie-form__label" htmlFor="director">
@@ -19,13 +60,20 @@ const MovieForm = (): React.ReactElement => {
           className="movie-form__input"
           required
           type="text"
+          onChange={onChangeData}
         />
       </div>
       <div className="movie-form__block">
         <label className="movie-form__label" htmlFor="imgUrl">
           Image URL:
         </label>
-        <input id="imgUrl" className="movie-form__input" required type="text" />
+        <input
+          id="imgUrl"
+          className="movie-form__input"
+          required
+          type="text"
+          onChange={onChangeData}
+        />
       </div>
       <Button text="Add Movie" />
     </MovieFormStyled>
